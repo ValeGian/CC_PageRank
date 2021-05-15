868fbf05bb4c9412bdbb85bcbdaaa2cc49520f64
ValeGian <46gianninivalerio@gmail.com>
2021-05-15 16:59:56 +0200

    changes InputFormat

--- CC_PageRank_Hadoop/src/main/java/it.unipi.cc.pagerank.hadoop/Parse.java
+++ CC_PageRank_Hadoop/src/main/java/it.unipi.cc.pagerank.hadoop/Parse.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -19,6 +20,9 @@ import java.util.regex.Pattern;
 
 public class Parse {
     public static class ParseMapper extends Mapper<LongWritable, Text, Text, Text> {
+        private final Text reducerKey = new Text();
+        private final Text reducerValue = new Text();
+
         public void map(final LongWritable key, final Text value, final Context context) throws IOException, InterruptedException {
             /*System.out.println("Map START - " + value.toString());
             Pattern p = Pattern.compile("\[[.*]]");
@@ -30,49 +34,44 @@ public class Parse {
             }
             System.out.println("Map END\n\n");
              */
-            context.write(new Text("Test Title"), new Text("Linked Page " + key.toString()));
+            reducerKey.set("Test Page Title");
+            reducerValue.set("Page " + key.toString());
+            System.out.println(">> Map writes [Page] \"" + reducerKey + "\t[Linked Page] \"" + reducerValue + "\"");
+            context.write(reducerKey, reducerValue);
         }
     }
 
     public static class ParseReducer extends Reducer<Text, Text, Text, GraphNode> {
-        private final Text reducerKey = new Text();
-
         public void reduce(final Text key, final Iterable<Text> values, final Context context) throws IOException, InterruptedException {
             GraphNode reducerOutValue = new GraphNode();
             reducerOutValue.setPageRank(2.0);
             for (Text value: values) {
              reducerOutValue.addAdjNode(value);
-             System.out.println(value.toString());
             }
-            reducerKey.set(key.toString()+"]]");
-            context.write(reducerKey, reducerOutValue);
+            context.write(key, reducerOutValue);
         }
     }
 
-    public static class TestParseMapper extends Mapper<LongWritable, Text, Text, Text> {
-        public void map(final LongWritable key, final Text value, final Context context) throws IOException, InterruptedException {
+    public static class TestRankMapper extends Mapper<Text, Text, Text, Text> {
+        public void map(final Text key, final Text value, final Context context) throws IOException, InterruptedException {
             GraphNode node = new GraphNode();
-            String record = value.toString();
+            node.setFromJson(value.toString());
 
-            int endKey = record.indexOf("]]");
-            String keyPage = record.substring(0, endKey);
-
-            int startValue = record.indexOf("{");
-            node.setFromJson(record.substring(startValue));
-
-            System.out.println(keyPage);
+            System.out.println(key);
             System.out.println(node.toPrintableString());
         }
     }
 
     public static void main(final String[] args) throws Exception {
         final Configuration conf = new Configuration();
+        conf.set("mapreduce.output.textoutputformat.separator", "]"); // changes output separator from \t to ]
+        //conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "]"); // changes input separator from \t to ]
         final Job job = new Job(conf, "Parse");
         job.setJarByClass(PageRank.class);
 
         // set mapper/reducer
-        //job.setMapperClass(ParseMapper.class);
-        job.setMapperClass(TestParseMapper.class);
+        job.setMapperClass(ParseMapper.class);
+        //job.setMapperClass(TestRankMapper.class);
         job.setReducerClass(ParseReducer.class);
 
         // define mapper's output key-value
@@ -84,9 +83,11 @@ public class Parse {
         job.setOutputValueClass(GraphNode.class);
 
         // define I/O
+        //KeyValueTextInputFormat.addInputPath(job, new Path(args[0])); // for TestRankMapper
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+        //job.setInputFormatClass(KeyValueTextInputFormat.class); // for TestRankMapper
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
